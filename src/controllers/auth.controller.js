bbb124749041517cdd28d666b6cd1305f51f4ad3
arshadvkb <arshadvkb118@gmail.com>
2025-10-28 12:46:23 +0530

    updated http status

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -79,7 +79,7 @@ const login = async (req, res) => {
     if (isMatch) {
       generateToken(existingUser._id, res);
       return res
-        .status(201)
+        .status(200)
         .json({ success: true, message: 'Login successful' });
     }
      else {
