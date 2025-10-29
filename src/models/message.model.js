1d9be117fe6a5e13ea761a9c32305f4e115971a7
arshadvkb <arshadvkb118@gmail.com>
2025-10-29 18:17:06 +0530

    message schema added

---
+++ src/models/message.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const mesaageSchema = mongoose.Schema(
+  {
+    senderid: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    recieverid: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+
+const Message = mongoose.model('Message',mesaageSchema);
+
+export { Message };
