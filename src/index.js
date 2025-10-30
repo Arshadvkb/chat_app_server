6d6d023bc5827c80682f4c7654a4ccbcdfc4d365
arshadvkb <arshadvkb118@gmail.com>
2025-10-30 20:25:35 +0530

    added cors cofigurations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from "cors"
 
 import { db } from './config/mongo.js';
 import authrouter from './routes/auth.route.js';
@@ -10,6 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+}))
 
 const port = process.env.PORT;
 db();
