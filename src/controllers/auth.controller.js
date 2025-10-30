79f5a2b1a2a37d2902a0a670cb453cb0e59b6505
arshadvkb <arshadvkb118@gmail.com>
2025-10-30 12:07:00 +0530

    added update profile and checking for authenticated user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -93,4 +93,39 @@ const logout = (req, res) => {
   }
 };
 
-export { register, login, logout };
+
+
+
+ const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: 'Profile pic is required' });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log('error in update profile:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+ const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log('Error in checkAuth controller', error.message);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+export { register, login, logout, updateProfile, checkAuth };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { login, logout, register } from '../controllers/auth.controller.js';
+import { checkAuth, login, logout, register, updateProfile } from '../controllers/auth.controller.js';
+import { protectRout } from '../middleware/auth.middleware.js';
 
 const authrouter = express.Router();
 
 authrouter.post('/register', register);
 authrouter.post('/login', login);
 authrouter.post('/logout', logout);
+authrouter.put('/update-profile', protectRout, updateProfile);
+authrouter.get('/check', protectRout, checkAuth);
 
 export default authrouter;
